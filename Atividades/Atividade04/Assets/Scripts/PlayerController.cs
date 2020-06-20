bb007870b5aa5288b20e46cbcb41ccaa064f1575
Rafael Araujo <rafael0910i@gmail.com>
2020-06-20 16:24:34 -0300

    ReUpando atividade 04

---
+++ Atividades/Atividade04/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using Mirror;
+
+using UnityEngine;
+
+public class PlayerController : NetworkBehaviour
+{
+    public float _moveSpeed = 0.1f;
+    public float _moveRotation = 1f;
+    public GameObject projetilPrefab;
+    public Transform arma;
+
+    private void Start()
+    {
+        Material material = GetComponent<Renderer>().material;
+        material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+
+    private void Update()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * _moveSpeed);
+        transform.Rotate(0, Input.GetAxis("Horizontal") * _moveRotation, 0f);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            CmdAtirar();
+    }
+
+    [Command]
+    private void CmdAtirar()
+    {
+        GameObject projectile = Instantiate(projetilPrefab, arma.position, this.transform.rotation);
+        NetworkServer.Spawn(projectile);
+    }
+}
---
+++ Atividades/Atividade04/Assets/Scripts/ProjetilController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class ProjetilController : NetworkBehaviour
+{
+    public float _moveSpeed = 20f;
+    
+    private void Start()
+    {
+        Destroy(this.gameObject, 1f);
+    }
+
+    private void Update()
+    {
+        transform.Translate(0, 0, Time.deltaTime * _moveSpeed);
+    }
+
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        NetworkServer.Destroy(gameObject);
+        NetworkServer.Destroy(other.gameObject);
+    }
+}
